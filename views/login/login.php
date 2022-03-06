f51bf55298df297a643637e45ab8f8bb38219c7c
trungquan21 <quantrung.2k1@gmail.com>
2022-03-06 13:34:51 +0700

    UI_login

---
+++ views/login/login.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, shrink-to-fit=no"
+    />
+    <title>Facebook</title>
+    <!--THƯ VIỆN BOOSTRAP-->
+    <link rel="stylesheet" href="../../assets/bootstrap/css/bootstrap.min.css" />
+    <link
+      rel="stylesheet"
+      href="../../assets/css/login-page/SF-Pro-Display-Bold.css?v=<?php echo time(); ?>"
+    >
+    <link
+      rel="stylesheet"
+      href="../../assets/css/login-page/SF-Pro-Display-Medium.css?v=<?php echo time(); ?>"
+    >
+    <link
+      rel="stylesheet"
+      href="../../assets/css/login-page/SF-Pro-Display-Regular.css?v=<?php echo time(); ?>"
+    >
+    <link rel="stylesheet" href="../../assets/css/login-page/login-page.css?v=<?php echo time(); ?>">
+  </head>
+
+
+<body>
+    <div id="body">
+        <div class=" form_login container">
+            <div class="">
+                <div id="body-row" class="row">
+                    <div class="col-xxl-1"></div>
+                    <div class="
+                    col-xxl-5 
+                    col-lg-6 col-12
+                    d-flex
+                    flex-column
+                    justify-content-center
+                    justify-content-lg-start
+                    align-items-center
+                    align-items-lg-start
+                    ">
+                        <div id="text-wrapper">
+                            <h1 style="color: #fff;text-align: center;">Log In</h1>
+                        </div>
+                        <div class="
+                    col-xxl-5
+                    col-lg-6
+                    col-12
+                    d-flex 
+                    flex-column
+                    justify-content-center
+                    justify-content-lg-start
+                    align-items-center
+                    align-items-lg-start
+                ">
+                            <div id="body-top-right">
+                                <form action="" method="post" class="form_layout">
+                                    <div class="" style="margin-bottom: 20px;">
+                                        <input name="UserEmail" class="form-control login-form-input" type="text"
+                                            placeholder="Email address" autofocus required />
+                                        <input name="UserPassword" id="login-form-input-password"
+                                            class="form-control login-form-input" type="password" placeholder="Password"
+                                            required />
+
+                                    </div>
+                                    <div class="
+                                        col-xxl-5
+                                        col-lg-6
+                                        col-12
+                                        justify-content-center
+                                        justify-content-lg-start
+                                        align-items-center
+                                        align-items-lg-start
+                                        checkbox">
+                                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                        <label class="form-check-label" for="exampleCheck1">Remember me</label>
+                                        <a class="needhelp_form" href="">Sign up?</a>
+                                    </div>
+                                    <button name="btnSignIn" id="login-btn" class="btn btn-primary btn-lg"
+                                        type="submit">
+                                        Log In Now
+                                    </button>
+                                    <div id="forgotten-pwd-link-wrapper" class="text-center">
+                                        <a id="forgotten-pwd-link" class="text-decoration-none" href="#">
+                                            Forgotten password?
+                                        </a>
+                                    </div>
+                            </div>
+
+
+                            </form>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
